e8ef0733e11a31a1c8c57d96832764a83d55631d
wrblr <wrbell@uchicago.edu>
2024-11-23 07:42:09 +0000

    only show forms if signed in user

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -26,7 +26,7 @@ class BoardsController < ApplicationController
   def create
     the_board = Board.new
     the_board.name = params.fetch("query_name")
-
+    the_board.user_id = current_user.id
     if the_board.valid?
       the_board.save
       redirect_to("/boards/#{the_board.id}", { :notice => "Board created successfully." })
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_23_071631) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_23_071739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_23_071631) do
     t.integer "board_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
